a6b52ef483e38e7ae26cb8f934618aec0fee89d3
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-09-21 10:49:13 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -15,9 +15,26 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
+
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter_by(id=id).first()
+
+    game_dict = game.to_dict()
+
+    response = make_response(
+        jsonify(game_dict),
+        200
+    )
+    response.headers["Content_Type"] = "application/json"
+
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
